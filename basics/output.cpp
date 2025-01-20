4854d15338c0c5881b1a6606b718ccf966079e27
rabobahago <rabobahago@gmail.com>
2025-01-20 10:24:09 +0100

    sum vector function product number added

--- basics/output.cpp
+++ basics/output.cpp
@@ -5,15 +5,22 @@ using namespace std;
 double x {23.34};
 complex<double>z{23.234};
 vector<int>vec {6, 5, 6};
+
 double sum_vector(const vector<int>&);
+constexpr double product_num(const double &);
+constexpr double product_num(const double &a){
+    return a * 2.0;
+}
 int main(){
     for(const auto& elem:vec){
         cout <<elem << endl;
 
     }
-    cout << sum_vector(vec);
+    constexpr double result = product_num(23);
+    cout << sum_vector(vec) + result;
     return 0;
 }
+
 double sum_vector(const vector<int>& vec){
     double count = 0;
     for(const auto& elem:vec){
