929c3b44038fc238fd5adde10d7b35b04f816b59
rabobahago <rabobahago@gmail.com>
2025-01-24 22:42:34 +0100

    abstraction and encapsulation for object oriented programming in c++ implemented

--- oop/oop.cpp
+++ oop/oop.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 using namespace std;
-class Employee{
-    public:
+class AbstractEmployee{
+    virtual void AskForPromotion() = 0;
+};
+class Employee: AbstractEmployee{
+    private:
     string Name;
     string Company;
     int Age;
-
+    public:
     Employee(string name, string company, int age){
         Name = name;
         Company = company;
         Age = age;
+    }
+    void setName(string name){
+        Name = name;
+    }
+
+    string getName(){
+        return Name;
+    }
+    void setAge(int age){
+        if (age >= 18)
+        Age = age;
+    }
+
+    int getAge(){
+        return Age;
+    }
+    void setCompany(string company){
+        Company = company;
+    }
+    string getCompany(){
+        return Company;
     }
     void IntroduceYourself(){
         cout << "Hello, my name is: " << Name <<"\n";
@@ -17,9 +41,16 @@ class Employee{
         cout << "I work for " << Company << "\n";
 
     }
+
+    void AskForPromotion() override{
+        if(Age >= 30)
+        cout << "hello, " << Name << ". Congratulations, You have been promoted" << "\n";
+        else
+        cout << "hello, " << Name << ". Sorry, You have not been promoted" << "\n";
+    }
 };
 int main(){
-    Employee employee1 = Employee("Rabo", "Apple Developer real", 23);
-    employee1.IntroduceYourself();
+    Employee employee1 = Employee("Rabo Yusuf", "Amazon Dev", 12);
+    employee1.AskForPromotion();
 
 }
